79c5615ecffa2fd07fcecc3c29791370b69b06cd
Jason Agosta <agostaj@d20green.com>
2017-02-26 16:43:26 -0500

    some edge cases

---
+++ tests/example/food/fridge/milk.js
@@ -0,0 +1,18 @@
+
+//@ provides com.sleepyjay.milk
+
+//# Self fulfilling...
+//@ provides com.sleepyjay.calcium
+//@ requires com.sleepyjay.calcium
+
+//# only 1 lemmon should be provided
+
+var Lemon;
+
+
+
+
+
+
+
+
--- tests/example/food/fridge/toppings/ketchup.js
+++ tests/example/food/fridge/toppings/ketchup.js
@@ -1,5 +1,6 @@
 
 //@ provides com.sleepyjay.ketchup
+//@ requires com.sleepyjay.ketchup
 
 var Ketchup;
 
